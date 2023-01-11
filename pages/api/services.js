2fa410159e79a729a1acf06867b0b6148be79f17
Brandon Luffman <brandonluff10@gmail.com>
2023-01-11 13:20:25 -0500

    File changes

--- pages/api/services.js
+++ pages/api/services.js
@@ -11,56 +11,56 @@ const serviceInfo = [
       Slug: "radon-inspection",
       Name: "Radon Inspection",
       Text: "A radon inspection is a test to measure the levels of radon gas in a building. Radon is a naturally occurring radioactive gas that can seep into buildings through foundation cracks and other openings. It is the leading cause of lung cancer among non-smokers and is responsible for about 21,000 deaths in the United States each year. During a radon inspection, a certified radon inspector will place a radon testing device in the lowest livable level of a building for a period of at least 48 hours. The device will measure the levels of radon gas in the air and provide a reading of the radon concentration. If the levels of radon are found to be high, the inspector may recommend further testing or the installation of a radon mitigation system to reduce the levels of gas in the building. It is important to have a radon inspection performed if you are buying or selling a home, or if you are concerned about the levels of radon in your current home.",
-      Image: "/radon.jpeg"
+      Image: "/radon.webp"
     },
     {
       Id: 2,
       Slug: "septic-inspection",
       Name: "Septic System Inspection",
       Text: "A septic inspection is an examination of a septic system to determine its condition and verify that it is functioning properly. This typically involves visually inspecting the various components of the system, including the septic tank, drain field, and any associated piping. The inspector may also check the levels of scum and sludge in the tank, and evaluate the condition of the soil in the drain field. The purpose of a septic inspection is to identify any problems or potential issues with the system so that they can be addressed before they lead to more serious problems.",
-      Image: "/septic.jpeg"
+      Image: "/septic.webp"
     },
     {
       Id: 3,
       Slug: "sewer-scoping",
       Name: "Sewer Scoping",
       Text: "Sewer scoping, also known as sewer line inspection or sewer camera inspection, is a process of visually inspecting the interior of a sewer line using a specialized camera. The camera is inserted into the sewer line through a small access point, such as a cleanout, and then advanced through the line while providing a live video feed of the interior of the pipe. During a sewer scoping, the inspector can identify any issues within the sewer line such as blockages, cracks, corrosion, or collapses. It also allows to locate potential problems like tree root intrusion, misaligned pipes and broken pipes. This can help property owners, plumbers and municipalities to determine the condition of the sewer line and plan for any necessary repairs or replacements. Sewer scoping is a non-invasive and cost-effective way to inspect sewer lines, and it can often be done without the need for digging or excavation.",
-      Image: "/sewer-scope.png"
+      Image: "/sewer-scope.webp"
     },
     {
       Id: 4,
       Slug: "water-quality-testing",
       Name: "Water Quality Testing",
       Text: "Water quality testing is the process of analyzing water samples to determine the presence, concentration, and/or identity of various chemical, physical, and biological characteristics. These characteristics may include parameters such as pH, temperature, turbidity, dissolved oxygen, bacteria, and various chemical pollutants. Water quality testing is important because it helps to determine whether water is safe to drink, swim in, or use for irrigation and other purposes. It also helps to identify and address any potential water pollution problems. Different types of water bodies may require different test methods depending on the source and the intended use of the water. For example, testing for bacteria and viruses is important for drinking water, while testing for nutrients and dissolved oxygen may be more relevant for surface waters like lakes and streams. Test results are compared against established standards, such as those set by the US Environmental Protection Agency (EPA), to determine if the water meets safety and quality standards. Test results can be used to identify any problems with the water quality and to develop strategies for addressing those issues.",
-      Image: "/water-test.jpeg"
+      Image: "/water-test.webp"
     },
     {
       Id: 5,
       Slug: "water-system-test",
       Name: "Water System Test",
       Text: "A water system test is a process of evaluating the performance and functionality of a water supply system. This can include testing the physical components of the system, such as pumps, valves, and pipes, as well as testing the water itself for quality and safety. There are several types of water system tests, including: Hydrostatic test: This test involves pressurizing the water system with water and then checking for leaks or other issues. This test is commonly used to check for leaks in new or renovated pipes or other components before they are put into service. Flow test: This test measures the flow rate and pressure of water through the system, and can help to identify any issues with the system's capacity or functionality. Water quality test: This test analyzes the water for various chemical, physical, and biological characteristics, as described in my previous answer. Bacterial test : this type of test is intended to verify the presence of bacteria such as E.coli, Coliforms and fecal coliforms in the water system to indicate its potential for contamination. Chemical test: This test measures the presence of various chemicals such as heavy metals, pesticides, and other pollutants in the water. The results of these tests help to identify any issues with the water supply system and can be used to develop strategies for addressing those issues. They also help to ensure that the system is functioning properly and that the water is safe for human consumption and other uses.",
-      Image: "/water-system.jpeg"
+      Image: "/water-system.webp"
     },
     {
       Id: 6,
       Slug: "well-inspection",
       Name: "Well Inspection",
       Text: "A well inspection is a process of evaluating the condition and functionality of a private well. The purpose of a well inspection is to ensure that the well is in good condition, that it is properly constructed and maintained, and that the water it produces is safe for human consumption and other uses. During a well inspection, a professional inspector will typically evaluate the following aspects of the well: The well structure: The inspector will check the well casing and cap for proper installation and any visible defects. They will also check that the well is properly sealed to prevent contamination from surface water or pollutants. Water flow and yield: The inspector will measure the wells water flow rate to ensure it is producing enough water for the needs of the household or building and evaluate if the flow rate is consistent. Water quality: A water sample will be collected and sent to a laboratory for analysis. The inspector will check for the presence of harmful contaminants such as bacteria, chemicals, or other pollutants that can affect the safety of the water. Electrical and mechanical system: The inspector will evaluate the electrical and mechanical system of the well, including the pump, pressure tank and any additional treatment equipment. They will also check that all systems are properly grounded to prevent electrical shock.The results of the inspection will be reported and can be used to identify any issues with the well, such as poor construction, water quality problems, or mechanical failures, and develop a plan for addressing those issues. It's important to note that well inspection should be done by professional and certified well inspector, and at regular intervals, like every 4-5 years or as recommended by local regulations. ",
-      Image: "/well-inspection.jpeg"
+      Image: "/well-inspection.webp"
     },
     {
       Id: 7,
       Slug: "wind-mitigation-inspection",
       Name: "Wind Mitigation Inspection",
       Text: "A wind mitigation inspection is an evaluation of a building's resistance to wind damage. The goal of a wind mitigation inspection is to identify structural and design features of a building that can help to reduce damage from high winds, such as those caused by hurricanes and other severe storms. During a wind mitigation inspection, a professional inspector will typically evaluate the following aspects of the building: Roofing system: The inspector will check the type of roof covering, the age of the roof, and the method of attachment of the roof deck to the framing. These factors can greatly impact the wind resistance of a building. Roof-to-wall connections: The inspector will check the connections between the roof and the walls of the building to ensure they are strong enough to resist wind uplift. Gable end bracing: The inspector will evaluate the bracing used to stabilize the gable ends of the building, which are particularly vulnerable to wind damage. Opening protection: The inspector will check the building's windows, doors, and garage doors to ensure they are equipped with adequate protection to prevent wind damage, such as shutters or impact-resistant glass. Other structural features: The inspector will evaluate other structural features of the building that can affect its wind resistance, such as the foundation, the shape of the building and the shape of the roof. Based on the inspection, the inspector will provide a report that includes a summary of the building's wind resistance features and any recommendations for improvement. This report can be used by homeowners and insurance companies to determine the building's potential wind damage, and to negotiate lower insurance premiums. It's important to note that wind mitigation inspections should be done by certified wind mitigation inspector and it's recommended to do it at the time of purchasing or selling a property or as the insurance company required it.",
-      Image: "/wind-inspect.jpeg"
+      Image: "/wind-inspect.webp"
     },
     {
       Id: 8,
       Slug: "insect-inspection",
       Name: "Wood Damaging Insect Inspection",
       Text: "A wood-damaging insect inspection is an evaluation of a structure to determine if it has been damaged by insects that feed on wood, such as termites, carpenter ants, and powderpost beetles. The goal of the inspection is to identify the presence of these pests and the extent of any damage caused by them. During a wood-damaging insect inspection, a professional inspector will typically evaluate the following aspects of the structure: Evidence of wood-damaging insects: The inspector will look for signs of wood-damaging insects, such as discarded wings, sawdust, mud tubes, and live or dead insects. Damage to the wood: The inspector will check for structural damage caused by wood-damaging insects, such as holes, hollow sounding wood, frass (insect excrement) and cracks in the wood. Presence of moisture: The inspector will check for signs of moisture in the structure, such as water leaks or high humidity, as these can attract wood-damaging insects. Previous treatments: The inspector will check the structure for any previous treatments that may have been done to address wood-damaging insects, such as chemical barriers or fumigations. Based on the inspection, the inspector will provide a report that includes a summary of the findings, including the presence or absence of wood-damaging insects, the extent of any damage, and any recommendations for treatment and repair. If the pests are found, the inspection will also include a recommendation of a course of action, such as treatment or removal of the pests. It's important to note that wood-damaging insect inspections should be done by certified inspector and at regular intervals, especially if you live in areas with high termite activity or if you suspect that you have an infestation.",
-      Image: "/insect.jpeg"
+      Image: "/insect.webp"
     },
   ];
     
